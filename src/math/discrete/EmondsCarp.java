d33d91e4938dca9e278a0da00383b89d0429e6ef
don-dron <ya.andreysavo@gmail.com>
2020-05-12 21:08:44 +0300

    Emonds Carp init

--- src/math/discrete/EmondsCarp.java
+++ src/math/discrete/EmondsCarp.java
@@ -14,5 +14,6 @@ public class EmondsCarp {
         for (Edge edge : residualNetwork.getEdges().stream().filter(edge -> edge.type == Edge.EdgeTypes.FORWARD).collect(Collectors.toList())) {
             iterator.next().flow = edge.flow;
         }
+        System.out.println(graph.getFlows());
     }
 }
